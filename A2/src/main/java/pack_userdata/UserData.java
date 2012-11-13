29fd2ff2ebae4afb1b30e3634bb45523495296f5
dsobol <dsobol@dsobol.prontosoft.by>
2012-11-13 18:30:21 +0400

    Первый коммит

---
+++ A2/src/main/java/pack_userdata/UserData.java
@@ -0,0 +1,13 @@
+package pack_userdata;
+
+public class UserData
+{
+	public String name;
+	public String password;
+	
+	UserData(String name, String password)
+	{
+		this.name=name;
+		this.password=password;
+	}
+}
