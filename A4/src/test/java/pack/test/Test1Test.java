d17951d91b9ca556ab2c18adb4eef543ccd4c5bc
Timon2180 <Timon2180@mail.ru>
2012-12-06 19:28:00 +0300

    34

--- A4/src/test/java/pack/test/Test1Test.java
+++ A4/src/test/java/pack/test/Test1Test.java
@@ -19,7 +19,7 @@ class Proper
 		{
 			prop.load(ClassLoader.getSystemResourceAsStream("p.properties"));
 		}
-		catch(IOException exc){exc.printStackTrace(); System.out.println("Не удалось загрузить файл конфигурации");}
+		catch(IOException exc){exc.printStackTrace(); System.out.println("Dont Work");}
 	}
 	
 	public static String GetProperty(String sKey)
