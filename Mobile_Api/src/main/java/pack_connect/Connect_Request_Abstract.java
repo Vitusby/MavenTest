3b3e901668a7cbce83e00beda246be4cb8b0ba04
Timon2180 <timon2180@mail.ru>
2013-01-30 14:18:01 +0300

    change pom and java

--- Mobile_Api/src/main/java/pack_connect/Connect_Request_Abstract.java
+++ Mobile_Api/src/main/java/pack_connect/Connect_Request_Abstract.java
@@ -117,7 +117,7 @@ public abstract class Connect_Request_Abstract
 	// ?param[value_param1]=value1
 	public final String CreateArrayRequest(String sMainParam, String sDataForArrayRequest) throws UnsupportedEncodingException
 	{
-		sDataForArrayRequest = sDataForArrayRequest.replaceAll(" ", "").replaceAll("^", "").replaceAll(",", ",[").replaceAll("=", "]=").replace("№", "[")
+		sDataForArrayRequest = sDataForArrayRequest.replaceAll(" ", "").replaceAll("}", "").replaceAll(",", ",[").replaceAll("=", "]=").replace("{", "[")
 				.replaceAll("\"", "");
 		String s1[] = sDataForArrayRequest.split(",");
 		
