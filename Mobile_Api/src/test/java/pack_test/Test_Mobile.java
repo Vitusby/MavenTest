90a9d53fcceb0fbd3c6a378ef3a5a7bffed03fb2
Timon2180 <timon2180@mail.ru>
2013-02-13 22:29:02 +0200

    edit pathq

--- Mobile_Api/src/test/java/pack_test/Test_Mobile.java
+++ Mobile_Api/src/test/java/pack_test/Test_Mobile.java
@@ -122,7 +122,7 @@ public class Test_Mobile
 	}
 	
 	
-	@Test (groups = { "AutoTest" })
+	@Test (groups = { "AutoTest_15" })
 	@Parameters({"sBaseHost"})
 	public void AutoTest_GetAndCheckRubricator(String sBaseHost) throws URISyntaxException, IOException, ExceptFailTest, JSONException, InterruptedException, ClassNotFoundException
 	{
@@ -130,7 +130,7 @@ public class Test_Mobile
 	}
 	
 	
-	@Test (groups = { "AutoTest" })
+	@Test (groups = { "AutoTest_16" })
 	@Parameters({"sBaseHost"})
 	public void AutoTest_GetFieldsForAddAdvert(String sBaseHost) throws URISyntaxException, IOException, ExceptFailTest, JSONException, InterruptedException, ClassNotFoundException
 	{
