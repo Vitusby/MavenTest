64c062d465d13093cf76a65640a6d334f48f1259
Timon2180 <timon2180@mail.ru>
2013-02-15 12:41:19 +0300

    change test

--- Mobile_Api/src/main/java/pack_connect/ConnectMethod.java
+++ Mobile_Api/src/main/java/pack_connect/ConnectMethod.java
@@ -4118,11 +4118,11 @@ public class ConnectMethod extends Connect_Request_Abstract
 		smas[5] = sCusTIYBookReaderKazan;
 
 		//Раскоментить если надо будет обновить значения и закомментить после обновления
-		//Js = new JString(smas); // запись полей для рубрик в файл
-		//SaveJson(Js, "CustomfieldsForSearch.txt");
+		Js = new JString(smas); // запись полей для рубрик в файл
+		SaveJson(Js, "CustomfieldsForSearch2.txt");
 		
 		
-		String sIdealSearchFields[] = LoadJson("CustomfieldsForSearch.txt");
+		String sIdealSearchFields[] = LoadJson("CustomfieldsForSearch2.txt");
 		
 		print("\r\nШАГ 2");
 		print("Сравниваем список полей рубрики для фильтрации Авто - Главная. Регион Москва, полученных запросом, со списком полей из сохранения".toUpperCase());
