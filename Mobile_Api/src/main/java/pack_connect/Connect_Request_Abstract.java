145013e622befc405272dd6635cc48de338f622f
Timon2180 <timon2180@mail.ru>
2013-02-16 20:32:09 +0200

    edit test

--- Mobile_Api/src/main/java/pack_connect/Connect_Request_Abstract.java
+++ Mobile_Api/src/main/java/pack_connect/Connect_Request_Abstract.java
@@ -86,16 +86,18 @@ public abstract class Connect_Request_Abstract
     	StringEntity se = new StringEntity(sBodyRequest, "UTF-8");
     	
     	post.setURI(uri);
-    	se.setContentType("application/x-www-form-urlencoded");
     	post.setEntity(se);
     	
-    	/*FileBody bin = new FileBody(new File(sPath));
+    	FileBody bin = new FileBody(new File(sPath));
         StringBody comment = new StringBody("Filename: Image" );
         MultipartEntity reqEntity = new MultipartEntity();
         reqEntity.addPart("image1", bin);
         reqEntity.addPart("comment", comment);
+        
+        se.setContentType("application/x-www-form-urlencoded");
+        
         post.setEntity(reqEntity);
-    	*/
+    	
     	
     	response = hClient.execute(post);
     	sTempResponse = GetContentResponse(response);
