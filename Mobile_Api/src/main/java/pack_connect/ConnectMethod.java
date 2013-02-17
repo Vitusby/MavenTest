c66005456164e08f57d24a406465b930b802a880
Timon2180 <timon2180@mail.ru>
2013-02-17 11:23:10 +0200

    edit test

--- Mobile_Api/src/main/java/pack_connect/ConnectMethod.java
+++ Mobile_Api/src/main/java/pack_connect/ConnectMethod.java
@@ -6663,33 +6663,29 @@ public class ConnectMethod extends Connect_Request_Abstract
 		print("ADVERTISEMENT_ID = "+ sIdAdvert);
 		print("sApp_token = "+ sApp_token);
 		builder = new URIBuilder();
-    	builder.setScheme("http").setHost(sHost).setPath("/mobile_api/1.0/advertisements/advert/" + sIdAdvert + "/pushup")
-    		.setParameter("auth_token", sAuth_token)
-    		.setParameter("app_token", sApp_token);;
+    	builder.setScheme("http").setHost(sHost).setPath("/mobile_api/1.0/advertisements/advert/" + sIdAdvert + "/pushup");
+    		
+    	String sE = "auth_token=" + sAuth_token + "&app_token=" + sApp_token;
+    	
     	uri = builder.build();
-    	if(uri.toString().indexOf("%25") != -1)
-    	{
-    		String sTempUri = uri.toString().replace("%25", "%");
-    		uri = new URI(sTempUri);			
-    	}
     	print("Отправляем запрос. Uri Запроса: "+uri.toString());
     	
 
-    	String sResponse = HttpPostRequest(uri);
+    	String sResponse = HttpPostRequest2(uri, sE);
     	print("Парсим ответ....");
     	
     	jsonObject = ParseResponse(sResponse);
     	if(jsonObject.isNull("error"))
     	{
     		if(jsonObject.toString().equals("{\"error\":null,\"actions\":false}"))
-    			print("Ответ сервера:" + jsonObject.toString() + " Объявление не поднято (возможно оно неактивно, неоплачено)");
+    			print("Ответ сервера:\r\n" + jsonObject.toString(10) + "\r\nОбъявление не поднято (возможно оно неактивно, неоплачено)");
     		else 
-    			print("Ответ сервера:" + jsonObject.toString() + " Объявление поднято");
+    			print("Ответ сервера:\r\n" + jsonObject.toString(10) + "\r\nбъявление поднято");
     	}
     	else
     	{
     		print("Не удалось поднять объявление \r\n"+
-    				"Ответ сервера:\r\n"+ jsonObject.toString());
+    				"Ответ сервера:\r\n"+ jsonObject.toString(10));
     		throw new ExceptFailTest("Тест провален");
     	}	
 	}
