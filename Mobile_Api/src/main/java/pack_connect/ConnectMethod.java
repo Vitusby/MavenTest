ffd7a87b5a28163e2861f89ab77fceee2181cc12
Timon2180 <timon2180@mail.ru>
2013-02-25 10:59:55 +0300

    change test

--- Mobile_Api/src/main/java/pack_connect/ConnectMethod.java
+++ Mobile_Api/src/main/java/pack_connect/ConnectMethod.java
@@ -2554,6 +2554,9 @@ public class ConnectMethod extends Connect_Request_Abstract
 		String mas3[] = mas[1].split(":"); 
 		c.set(Integer.parseInt(mas2[0]), Integer.parseInt(mas2[1]), Integer.parseInt(mas2[2]), Integer.parseInt(mas3[0]), Integer.parseInt(mas3[1]), Integer.parseInt(mas3[2]));
 		long l = c.getTimeInMillis();
+		print("---------------------------------------");
+		print("Время продления объявления "+ l);
+		print("---------------------------------------");
 		return l;
 	}
 	// выделения объявления для автотеста
